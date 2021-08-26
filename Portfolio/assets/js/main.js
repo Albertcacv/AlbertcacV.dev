24f978df39cd723d58593bd6fbe5faf863324245
albert <albertcacv@gmail.com>
2021-08-26 14:40:58 +0200

    add home refactor

---
+++ Portfolio/assets/js/main.js
@@ -0,0 +1,21 @@
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+// Remove menu mobile
+
+navMenu.addEventListener('click', (e) => {
+  if (e.target.className == 'nav__link') navMenu.classList.remove('show-menu')
+})
