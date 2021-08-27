38fcd11b0f1a3d1e34b74d82228f59789ff4965c
albert <albertcacv@gmail.com>
2021-08-27 17:05:59 +0200

    add qualifcation and skills refactor

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -19,3 +19,37 @@ if (navClose) {
 navMenu.addEventListener('click', (e) => {
   if (e.target.className == 'nav__link') navMenu.classList.remove('show-menu')
 })
+
+//Accordion skills
+const skillsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+
+  if (itemClass == 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach((el) => el.addEventListener('click', toggleSkills))
+
+//Swiper slide
+const swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+})
