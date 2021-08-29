7a5653df83a1b0d4b993e1331d069eda3f86531d
albert <albertcacv@gmail.com>
2021-08-29 20:33:23 +0200

    add minor fixes

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -14,8 +14,6 @@ if (navClose) {
   })
 }
 
-
-
 // Remove menu mobile
 navMenu.addEventListener('click', (e) => {
   if (e.target.className == 'nav__link') navMenu.classList.remove('show-menu')
@@ -36,24 +34,43 @@ function toggleSkills() {
     this.parentNode.className = 'skills__content skills__open'
   }
 }
-
 skillsHeader.forEach((el) => el.addEventListener('click', toggleSkills))
 
+// QUALIFICATION TABS
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContent = document.querySelectorAll('[data-content]')
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+    tabContent.forEach((tabContent) => {
+      tabContent.classList.remove('qualification__active')
+    })
+
+    target.classList.add('qualification__active')
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})
+
 //Swiper slide
-// let swiper = new Swiper('.portfolio__container', {
-//   cssMode: true,
-//   loop: true,
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//   mousewheel: true,
-//   keyboard: true,
-// })
+let swiper = new Swiper('.portfolio__container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+})
 
 // SCROLL SECTIONS ACTIVE LINK
 const sections = document.querySelectorAll('section[id]')
