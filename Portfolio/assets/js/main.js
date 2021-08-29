c1784c8315b91cea64139d9c656c5d70a9304926
albert <albertcacv@gmail.com>
2021-08-29 14:22:50 +0200

    add media queries

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -15,7 +15,6 @@ if (navClose) {
 }
 
 // Remove menu mobile
-
 navMenu.addEventListener('click', (e) => {
   if (e.target.className == 'nav__link') navMenu.classList.remove('show-menu')
 })
@@ -39,17 +38,79 @@ function toggleSkills() {
 skillsHeader.forEach((el) => el.addEventListener('click', toggleSkills))
 
 //Swiper slide
-const swiper = new Swiper('.swiper-container', {
-  cssMode: true,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  mousewheel: true,
-  keyboard: true,
+// let swiper = new Swiper('.portfolio__container', {
+//   cssMode: true,
+//   loop: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// })
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    let sectionId = current.getAttribute('id')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+// CHANGE BACKGROUND HEADER
+window.addEventListener('scroll', () => {
+  const nav = document.getElementById('header')
+  this.scrollY >= 80
+    ? nav.classList.add('scroll-header')
+    : nav.classList.remove('scroll-header')
+})
+
+// DARK THEME
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  )
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+    iconTheme
+  )
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme)
+  localStorage.setItem('selected-icon', getCurrentIcon)
 })
