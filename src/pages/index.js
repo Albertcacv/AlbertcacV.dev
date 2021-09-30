c0d528d6d621e5560e951e882f1111d4b5703a30
albert <albertcacv@gmail.com>
2021-09-30 16:13:17 +0200

    add skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import Header from '../components/Header'
 import Home from '../components/Home'
 import About from '../components/About'
 import Skills from '../components/Skills'
-import Qualification from '../components/Qualification'
 import Portfolio from '../components/Portfolio'
 import Contact from '../components/Contact'
 
@@ -24,10 +23,9 @@ const IndexPage = () => {
       <Header />
       <Home />
       <About />
+      <Skills />
 
       {/* 
-      <Skills />
-      <Qualification />
       <Portfolio />
       <Contact /> 
       */}
