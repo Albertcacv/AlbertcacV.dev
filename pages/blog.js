e38eb6e5026d6bd4f1ecc48d0f7f969d9b8f6cde
albert <albertcacv@gmail.com>
2022-03-04 02:12:13 +0100

    update from Gatsby to Next

---
+++ pages/blog.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Blog() {
+  return (
+    <div>
+      <Head>
+        <title>AlbertcacV | Blog</title>
+        <meta name='description' content='Blog site of AlbertcacV' />
+        
+      </Head>
+      <h1>BLOG</h1>
+    </div>
+  )
+}
