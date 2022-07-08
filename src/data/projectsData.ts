a2c8572f7c7a3f82c93afca03210fd9add955f7c
albert <55817155+Albertcacv@users.noreply.github.com>
2022-07-08 03:51:18 +0200

    Update projectsData.ts

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -3,7 +3,7 @@ export const projectsData = [
 		id: 1,
 		title: 'Rick and Morty API',
 		description: 'A simple API based on Rick and morty tv show.',
-		image: '/public/R&M_preview.png',
+		image: '/R&M_preview.png',
 		viewLink: 'https://rick-morty-cast-lime.vercel.app/',
 		codeLink: 'https://github.com/Albertcacv/RickMorty_Api',
 	},
