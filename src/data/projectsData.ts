ebef1506ef86195b4f71e19176ae19c261f02e91
albert <55817155+Albertcacv@users.noreply.github.com>
2022-07-16 14:35:13 +0200

    ⚒️ add new project

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,10 +1,10 @@
 export const projectsData = [
 	{
 		id: 1,
-		title: 'Rick and Morty API',
-		description: 'A simple API based on Rick and morty tv show.',
-		image: '/R&M_preview.png',
-		viewLink: 'https://rick-morty-cast-lime.vercel.app/',
+		title: 'Gym website template',
+		description: 'Landing of a gym platform created with React framework',
+		image: '/gym-website.png',
+		viewLink: 'https://gym-website-xi.vercel.app/',
 		codeLink: 'https://github.com/Albertcacv/RickMorty_Api',
 	},
 ];
